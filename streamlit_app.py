406d6c1a9de83978893732265d3335f1a93a0f7d
Bruno Gubler <144129504+brunogubler@users.noreply.github.com>
2023-09-11 15:15:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header ("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 # streamlit.text("Hello from Snowflake:")
+
+add_my_fruit = streamlit.text_input('Add a fruit to the List','jackfruit')
+
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
